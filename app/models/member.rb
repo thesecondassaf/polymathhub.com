defe08c7ec5f3ecb6c16a32cb4a9f9a472b0ffd8
polymathhub <mrznasf@gmail.com>
2020-06-12 15:02:45 +0300

    added devise model configs to member model

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,10 @@
 class Member < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable
+
   has_many :projects, inverse_of: 'owner', foreign_key: 'owner_id'
 
   has_many :participations
